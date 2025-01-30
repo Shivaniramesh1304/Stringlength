ce0a9aee2425ffc0afeb52d35e56b6c1c38cd070
Shivaniramesh1304 <shivaniramesh1304@gmail.com>
2025-01-30 21:59:10 +0530

    Add files via upload

---
+++ Stringlength.java
@@ -0,0 +1,8 @@
+public class Stringlength {
+    public static void main(String[]args){
+        String str="hello,world!";
+        int length=str.length();
+        System.out.println("the length of String is:"+ length);
+    }
+    
+}
